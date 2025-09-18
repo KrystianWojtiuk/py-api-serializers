2811a9566a157e9699231bf6a5279eb8471689c1
Krystian Wojtiuk <krystianwwork@gmail.com>
2025-09-18 16:01:10 +0100

    Fix urls for tests

--- cinema/urls.py
+++ cinema/urls.py
@@ -7,11 +7,11 @@ from cinema.views import (CinemaHallViewSet, GenreViewSet, ActorViewSet,
 app_name = "cinema"
 
 router = routers.DefaultRouter()
-router.register("cinema-halls", CinemaHallViewSet)
-router.register("genres", GenreViewSet)
-router.register("actors", ActorViewSet)
-router.register("movies", MovieViewSet)
-router.register("movie-sessions", MovieSessionViewSet)
+router.register("cinema_halls", CinemaHallViewSet, basename="cinemahall")
+router.register("genres", GenreViewSet, basename="genre")
+router.register("actors", ActorViewSet, basename="actor")
+router.register("movies", MovieViewSet, basename="movie")
+router.register("movie_sessions", MovieSessionViewSet, basename="moviesession")
 
 urlpatterns = [
     path("", include(router.urls))
--- cinema_service/urls.py
+++ cinema_service/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
+import cinema.urls
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/cinema/", include("cinema.urls", namespace="cinema")),
+    path("api/cinema/", include((cinema.urls, "cinema"), namespace="cinema")),
 ]
