164eeb6809a91455c2089f2c288bab155a427997
Krystian Wojtiuk <krystianwwork@gmail.com>
2025-09-18 14:47:17 +0100

    Fix serializers fields

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -6,25 +6,25 @@ from cinema.models import CinemaHall, Genre, Actor, Movie, MovieSession
 class CinemaHallSerializer(ModelSerializer):
     class Meta:
         model = CinemaHall
-    fields = '__all__'
+        fields = '__all__'
 
 
 class GenreSerializer(ModelSerializer):
     class Meta:
         model = Genre
-    fields = '__all__'
+        fields = '__all__'
 
 
 class ActorSerializer(ModelSerializer):
     class Meta:
         model = Actor
-    fields = '__all__'
+        fields = '__all__'
 
 
 class MovieSerializer(ModelSerializer):
     class Meta:
         model = Movie
-    fields = '__all__'
+        fields = '__all__'
 
 
 class MovieSessionSerializer(ModelSerializer):
