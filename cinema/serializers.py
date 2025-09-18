aa7b0c06ecf549c38f7f810ff490fc4016bba1c4
Krystian Wojtiuk <krystianwwork@gmail.com>
2025-09-18 14:37:14 +0100

    Add basic serializers and views

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -1,1 +1,35 @@
-# write serializers here
+from rest_framework.serializers import ModelSerializer
+
+from cinema.models import CinemaHall, Genre, Actor, Movie, MovieSession
+
+
+class CinemaHallSerializer(ModelSerializer):
+    class Meta:
+        model = CinemaHall
+    fields = '__all__'
+
+
+class GenreSerializer(ModelSerializer):
+    class Meta:
+        model = Genre
+    fields = '__all__'
+
+
+class ActorSerializer(ModelSerializer):
+    class Meta:
+        model = Actor
+    fields = '__all__'
+
+
+class MovieSerializer(ModelSerializer):
+    class Meta:
+        model = Movie
+    fields = '__all__'
+
+
+class MovieSessionSerializer(ModelSerializer):
+    class Meta:
+        model = MovieSession
+    fields = '__all__'
+
+
--- cinema/views.py
+++ cinema/views.py
@@ -1,1 +1,31 @@
-# write views here
+from rest_framework.serializers import ModelSerializer
+from rest_framework.viewsets import ModelViewSet
+
+from cinema.models import CinemaHall, Genre, Actor, Movie, MovieSession
+from cinema.serializers import CinemaHallSerializer, GenreSerializer, ActorSerializer, MovieSessionSerializer, \
+    MovieSerializer
+
+
+class CinemaHallViewSet(ModelViewSet):
+    queryset = CinemaHall.objects.all()
+    serializer_class = CinemaHallSerializer
+
+
+class GenreViewSet(ModelViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+
+
+class ActorViewSet(ModelViewSet):
+    queryset = Actor.objects.all()
+    serializer_class = ActorSerializer
+
+
+class MovieViewSet(ModelViewSet):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+
+
+class MovieSessionViewSet(ModelViewSet):
+    queryset = MovieSession.objects.all()
+    serializer_class = MovieSessionSerializer
